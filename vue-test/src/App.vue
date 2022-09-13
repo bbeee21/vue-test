6d816066173d9400169875b82cfe0f4d9dc84a04
임정웅 <bbeee21@hanyang.ac.kr>
2022-09-13 11:09:16 +0900

    feat1 v-for

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 
-  <span>안녕하세요</span>
+  <div class="menu">
+    <!-- <a>Home</a>
+    <a>Products</a>
+    <a>About</a> -->
+
+    <a v-for="i in menus" :key="i">{{ i}}</a>
+  </div>
+
+  <div v-for="i in products" :key="i" >{{ i }}
+    <!-- h4 태그에도 독같이 v-for 적용 가능함 -->
+    <!-- 전체 형태로 반복하기 위해서는 div 태그 위에다가 하기 -->
+    <!-- <h4 class="red" :style = "OneRoomStyle"> {{ products[0] }} </h4> -->
+    <p>{{ price[0] }}</p>
+  </div>
+  <!-- <div>
+    <h4> {{ products[1] }} </h4>
+    <p>{{ price[1] }}</p>
+  </div>
+  <div>
+    <h4> {{ products[2] }} </h4>
+    <p>{{ price[2] }}</p>
+  </div> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      menus : ['Home', 'Shop', 'About'],
+      products : ['과천동원룸','역삼동원룸','논현동원룸'],
+      price : [40,50,20],
+      OneRoomStyle : 'color : blue',
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -24,5 +51,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background : rgb(14, 169, 118);
+  padding : 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color : whitesmoke;
+  padding : 15px;
+}
+
+div {
+  color : red;
 }
 </style>
