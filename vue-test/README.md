786bb05575a1b9e2e334bc5d297d47daa12cbee4
임정웅 <bbeee21@hanyang.ac.kr>
2022-09-14 09:56:03 +0900

    build1 router 기능추가

--- vue-test/README.md
+++ vue-test/README.md
@@ -22,3 +22,13 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+## vue router
+vue add router
+// history mode : yes
+npm install vue-router --save
+
+## version info
+"vue": "^3.2.13",
+"vue-router": "^4.1.5"
--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -1,58 +1,10 @@
 <template>
-  <img alt="GS EPS LOGO" src="./assets/logo_gseps.jpg" class="gs_logo">
-  <Headers ></Headers>
-    <div>
-      <!-- h4 태그에도 독같이 v-for 적용 가능함 -->
-      <!-- 전체 형태로 반복하기 위해서는 div 태그 위에다가 하기 -->
-      <img src="https://ic.zigbang.com/ic/items/33259061/1.jpg?w=0&h=640&a=1" class="room-image">
-      <h4 class="red" :style = "OneRoomStyle"> {{ products[0] }} </h4>
-      <p>{{ price[0] }}만원</p>
-      <button @click="신고수[0]++">허위매물신고</button><div> 신고수 : {{ 신고수[0] }}</div>
-    </div>
-
-    <div>
-      <h4> {{ products[1] }}</h4>
-      <p>{{ price[1] }} 만원</p>
-      <button @click="신고수[1]++">허위매물신고</button><div> 신고수 : {{ 신고수[1] }}</div>
-    </div>
-
-    <div>
-      <h4> {{ products[2] }} </h4>
-      <p>{{ price[2] }} 만원</p>
-      <button @click="신고수[2]++">허위매물신고</button><div> 신고수 : {{ 신고수[2] }}</div>
-    </div>
-
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import Headers from './components/layout/Header.vue';
-
-export default {
-  name: 'App',
-  data(){
-    return {
-      신고수 : [0,0,0],
-      products : ['과천동원룸','역삼동원룸','논현동원룸'],
-      price : [40,50,20],
-      OneRoomStyle : 'color : blue',
-    }
-  },
-  components: {
-    Headers
-    
-  },
-  methods : {
-    increase(){
-      this.신고수 += 1;
-    },
-    sale(){
-      this.price[1] -= 2;
-      this.count += 1;
-    }
-  }
-
-}
-</script>
 
 <style>
 #app {
@@ -61,17 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-
-
-.room-image {
-  width : 10%;
-  margin-top : 40px;
+nav {
+  padding: 30px;
 }
 
-.gs_logo {
-  width : 20%;
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
